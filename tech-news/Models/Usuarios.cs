a76b59191a49e77d30fc0cdcb3a3b1216fca71fa
Pedroaguilar1232 <pedroaguilar1232@gmail.com>
2023-10-04 15:25:47 -0300

    implementação do CRUD para a classe Usuários

--- tech-news/Models/Usuarios.cs
+++ tech-news/Models/Usuarios.cs
@@ -8,10 +8,13 @@ namespace Tech_news.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required(ErrorMessage ="Obrigatório informar o nome!")]
         public string Nome { get; set; }
+        
         [Required(ErrorMessage = "Obrigatório informar e-mail!")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "Obrigatório informar a senha!")]
         public string Senha { get; set; }
     }
