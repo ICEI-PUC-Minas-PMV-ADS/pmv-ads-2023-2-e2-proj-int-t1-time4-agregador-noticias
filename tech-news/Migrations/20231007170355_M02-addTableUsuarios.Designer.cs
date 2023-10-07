1eb7ea740832ad755ddd43f7677b3920300967e9
Pedro Batista <pedro@yellowberry.com.br>
2023-10-07 14:21:30 -0300

    adicionado elemento perfil

--- tech-news/Migrations/20231007170355_M02-addTableUsuarios.Designer.cs
+++ tech-news/Migrations/20231007170355_M02-addTableUsuarios.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tech_news.Models;
 
@@ -10,9 +11,10 @@ using Tech_news.Models;
 namespace Tech_news.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231007170355_M02-addTableUsuarios")]
+    partial class M02addTableUsuarios
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
