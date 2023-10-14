2d94bdee4a951963c0a7466a77e9b9e1434b17be
pauloromano3738 <pauloromano3738@gmail.com>
2023-10-14 07:35:02 -0300

    configuração do banco de dados e da table noticia

---
+++ tech-news/Migrations/20231014102607_M03-addTableNoticia.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tech_news.Migrations
+{
+    public partial class M03addTableNoticia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Noticias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<int>(type: "int", nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Noticias", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Noticias");
+        }
+    }
+}
--- tech-news/Models/Noticia.cs
+++ tech-news/Models/Noticia.cs
@@ -1,6 +1,18 @@
-﻿namespace Tech_news.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tech_news.Models
 {
+    [Table("Noticias")]
     public class Noticia
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o título!")]
+        public int Titulo { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o link!")]
+        public string Link { get; set; }
     }
 }
