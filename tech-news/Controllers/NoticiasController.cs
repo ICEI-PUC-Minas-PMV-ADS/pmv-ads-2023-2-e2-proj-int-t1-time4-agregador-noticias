279b3f4fd2546cb4b83391c6885216a1e7758029
pauloromano3738 <pauloromano3738@gmail.com>
2023-10-14 08:08:30 -0300

    controller e view da notícia

---
+++ tech-news/Controllers/NoticiasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tech_news.Models;
+
+namespace Tech_news.Controllers
+{
+    public class NoticiasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public NoticiasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Noticias.ToListAsync();
+            return View(dados);
+        }
+    }
+}
