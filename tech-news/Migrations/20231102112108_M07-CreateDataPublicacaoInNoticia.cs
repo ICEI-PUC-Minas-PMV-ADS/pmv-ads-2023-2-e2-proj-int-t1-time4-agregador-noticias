581c0ac1435e4559c33b539eee49243e78c7a6d7
pauloromano3738 <pauloromano3738@gmail.com>
2023-11-02 08:23:27 -0300

    adicionando migration

---
+++ tech-news/Migrations/20231102112108_M07-CreateDataPublicacaoInNoticia.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tech_news.Migrations
+{
+    public partial class M07CreateDataPublicacaoInNoticia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataPublicacao",
+                table: "Noticias",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(System.DateTime.Now.Ticks, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataPublicacao",
+                table: "Noticias");
+        }
+    }
+}
